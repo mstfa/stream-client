265e44001033c8d7888163d957ad25bf480c0c99
mstfa <mustafatopcu54@mail.com>
2019-01-14 17:59:46 +0300

    Routes added

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import { BrowserRouter,Route , Link} from 'react-router-dom';
+import StreamCreate from './streams/StreamCreate';
+import StreamEdit from './streams/StreamEdit';
+import StreamShow from './streams/StreamShow';
+import StreamList from './streams/StreamList';
+import StreamDelete from './streams/StreamDelete';
 
 const App = () => {
-    return <div>App</div>;
-}
+    return (
+        <div>
+            <BrowserRouter>
+               <div>
+                   <Route path="/" exact component={StreamList} />
+                   <Route path="/streams/new"  component={StreamCreate} />
+                   <Route path="/streams/edit"  component={StreamEdit} />
+                   <Route path="/streams/delete"  component={StreamDelete} />
+                   <Route path="/streams/show"  component={StreamShow} />
+               </div>
+            </BrowserRouter>
+        </div>
+    );
+};
 
 export default App;
