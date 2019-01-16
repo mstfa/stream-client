b6a8e91d2bd60b36e939c083bbf09eacb9cb6518
MUSTAFA TOPÇU <mustafatopcu54@gmail.com>
2019-01-16 23:19:32 +0300

    Reducers

---
+++ src/reducers/streamReducer.js
@@ -0,0 +1,22 @@
+import _ from 'lodash';
+import {
+    FETCH_STREAM,
+    FETCH_STREAMS,
+    CREATE_STREAM,
+    EDIT_STREAM,
+    DELETE_STREAM
+} from '../actions';
+import streams from '../api/streams';
+
+export default (state={},action) => {
+    switch (action.type) {
+        case FETCH_STREAM:
+        case CREATE_STREAM:
+        case EDIT_STREAM:
+            return {...state,[action.payload.id]:action.payload};
+        case DELETE_STREAM:
+            return _.omit(state,action.payload);
+        default:
+           return state;
+    }
+}
