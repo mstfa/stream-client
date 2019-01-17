97a2827b10e76c48673af9879eb218b8fbedaee6
mstfa <mustafatopcu54@mail.com>
2019-01-17 11:37:54 +0300

    Before delete I completed Create Edit with resue form

--- src/reducers/streamReducer.js
+++ src/reducers/streamReducer.js
@@ -5,11 +5,12 @@ import {
     CREATE_STREAM,
     EDIT_STREAM,
     DELETE_STREAM
-} from '../actions';
-import streams from '../api/streams';
+} from '../actions/types';
 
 export default (state={},action) => {
     switch (action.type) {
+        case FETCH_STREAMS:
+             return {...state,..._.mapKeys(action.payload,'id')};
         case FETCH_STREAM:
         case CREATE_STREAM:
         case EDIT_STREAM:
